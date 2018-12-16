7f594687705dfada7de2914ffd956aeb28d89f2e
andrenaught <andrenaught@gmail.com>
2018-12-16 13:22:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ dataset.columns = ["ID", "Age", "Gender", "Education", "Country", "Ethnicity", "
 #print (dataset)
 
 #picking features set to choose from (this is not feature selection)
-personality_only = [5,6,7,8,9,10,11]
+personality_only = [6,7,8,9,10,11,12]
 all_x = [1,2,3,4,5,6,7,8,9,10,11,12]
 all_except_alcohol = [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 independent_cols = personality_only
